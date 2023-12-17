d6a9a4259e6ce1ab5b682e9616ac8b5d810989df
Getkart-Nonish <nonish.jain@getkart.com>
2023-12-17 17:42:45 +0530

    add example env

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NextJs_admin_panel
-Create Admin Dashboard Demo using NextJs  with Server Actions and dynamic API.
+
+Create Admin Dashboard Demo using NextJs with Server Actions and dynamic API.
+
+## Homepage
+
+http://localhost:9000/
+
+## Login
+
+http://localhost:9000/Login
+
+## Dashboard
+
+http://localhost:9000/dashboard
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,22 @@
+import styles from "@/app/home.module.css";
+import Link from "next/link";
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div className={styles.container}>
+      <h1>Welcome to NextJs Dashboard Project</h1>
+      <h3>This project is created by using NextJs with server component and Next-auth for authentication.</h3>
+      <Link href="/login">
+        <button className={styles.button}>Go to login page</button>
+      </Link>
+      <span>Project link :&nbsp;
+        <a href="https://github.com/Nonish/NextJs_admin_panel"
+          style={{ textDecoration: "underline" }}>
+          Github Repository
+        </a>
+      </span>
+    </div >
   )
 }
 
-export default Homepage
+export default Homepage;
